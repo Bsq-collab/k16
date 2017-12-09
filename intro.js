f01b3cd86bfb5bf0e2e6c2a8c84b1d72c7b8a80b
Bayan Berri <bberri@stuy.edu>
2017-12-09 17:09:29 -0500

    actually appends child and prints it

--- intro.js
+++ intro.js
@@ -2,14 +2,46 @@
 //  SoftDev1 pd7
 //  K15 -- DOMinating JS
 //  2017-12-11
+/* NOTES
+JS for selecting HTML elements from the dom:
+  document.getElementById(ID)
+  document.getElementByTagName(TAG)
+  document.getElementsbyCLassName(class)
+  document is like the root/computer/c:
+  JS for manipulating HTML elements
 
-var list=['bayan','bob','balloon','barbie','blue','bootstrap','bees','bright', 'bubbles'];
+  ELEMENT.remove()
+  ELEMENT.innerHTML(HTML)
+  document.createElement(html, tag, name)
+  ELEMENT. appendChild(element)
+  element.setAttribute(name,value)
+  element.getAttribute(mname)
+  element.addEventListener(event,function)
 
+*/
+
+//var list=['bayan','bob','balloon','barbie','blue','bootstrap','bees','bright', 'bubbles'];
+var listNum=8;//to add to the list the next number
+
+var addElementCallback=function(e){
+  var list=document.getElementById("thelist");
+  var newElem=document.createElement("li");
+  newElem.innerHTML='item '+ listNum;//item 8
+  list.appendChild(newElem);
+  console.log("appended child: "+ listNum);
+  listNum+=1;
+};
+
+var addElementButton = document.getElementById('b');
+addElementButton.addEventListener('click', addElementCallback);
+
+/*
 var addElement = function(list, el){
     list.push(el);
     //console.log(el + " has just been pushed to the list (" + list + ")");
     return list;
 };
+
 
 //Callback button for randomStudent
 //Returns 1 (one) random name from the list
@@ -22,7 +54,16 @@ var addElementCallback = function(e){
 var addElementButton = document.getElementById('b');
 addElementButton.addEventListener('click', addElementCallback);
 
+var mouseOverCallback=function(e){
+  var heading= document.getElementbyID('h');
+  heading.innerHTML= this.innerHTML;
+};
 
+var returnCallback=function(e){
+  var heading= document.getElementbyID('h');
+  heading.innerHTML="Hello World!";
+};
+*/
 
 //SIGNING OFF
 console.log("This has been a production by Bayan & Ibnul");
